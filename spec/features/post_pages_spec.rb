78c5e37ddd040555767d2ffb34f87fe0fb5fb1a0
Josue Valverde & Jessica Joly <student@epicodus.com>
2015-10-13 15:39:46 -0700

    Add integration specs for delete and edit posts.

--- spec/features/post_pages_spec.rb
+++ spec/features/post_pages_spec.rb
@@ -19,3 +19,28 @@ describe 'the add a post process' do
     expect(page).to have_content "can't be blank"
   end
 end
+
+describe 'the delete a post process' do
+  it "deletes a post" do
+    visit posts_path
+    click_on "Add Post"
+    fill_in "Title", :with => "Twitter Sucks"
+    fill_in "Url", :with => "www.twitter.com"
+    click_on "Create Post"
+    click_on "Delete"
+    expect(page).to_not have_content 'Twitter Sucks'
+  end
+end
+
+describe 'the edit a post process' do
+  it "edits a post" do
+    visit posts_path
+    click_on "Add Post"
+    fill_in "Title", :with => "Twitter Sucks Alot"
+    fill_in "Url", :with => "www.twitter.com"
+    click_on "Create Post"
+    click_on "Edit"
+    fill_in "Title", :with => "Twitter Sucks"
+    expect(page).to have_content 'Sucks'
+  end
+end
