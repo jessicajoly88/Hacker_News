79771095e800e1fb60d4c4d11ba6210ed98a50c9
Josue Valverde & Jessica Joly <student@epicodus.com>
2015-10-13 11:08:02 -0700

    Add return error message when title or url is left blank, integration spec pass.

--- spec/features/post_pages_spec.rb
+++ spec/features/post_pages_spec.rb
@@ -9,4 +9,13 @@ describe 'the add a post process' do
     click_on "Create Post"
     expect(page).to have_content 'Twitter Sucks'
   end
+
+  it "returns an errors message when title and url are left blank" do
+    visit posts_path
+    click_on "Add Post"
+    fill_in "Title", :with => " "
+    fill_in "Url", :with => " "
+    click_on "Create Post"
+    expect(page).to have_content "can't be blank"
+  end
 end
