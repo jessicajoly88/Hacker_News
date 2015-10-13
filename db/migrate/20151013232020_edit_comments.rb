9448849cd210e9fca4e9290e850384b57469aa34
Josue Valverde & Jessica Joly <student@epicodus.com>
2015-10-13 16:56:52 -0700

    Add comments relationship etc. functionality.

---
+++ db/migrate/20151013232020_edit_comments.rb
@@ -0,0 +1,5 @@
+class EditComments < ActiveRecord::Migration
+  def change
+    add_column(:comments, :post_id, :integer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013224109) do
+ActiveRecord::Schema.define(version: 20151013232020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151013224109) do
     t.integer  "point"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
