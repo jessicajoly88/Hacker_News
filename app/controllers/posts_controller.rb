9b3e423d21c11794109f28c3fe6fd42f25bf9c7d
Josue Valverde & Jessica Joly <student@epicodus.com>
2015-10-13 11:43:13 -0700

    add edit and delete functionality, along with bootstrap.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    render :index
   end
 
   def show
     @post = Post.find(params[:id])
-    render :show
   end
 
   def new
     @post = Post.new
-    render :new
   end
 
   def create
@@ -22,6 +19,25 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 private
